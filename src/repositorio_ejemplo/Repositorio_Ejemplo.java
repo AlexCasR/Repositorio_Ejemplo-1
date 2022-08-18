3650ec2a069aa2c9783e4ef3b636da49b70e5762
unknown <alexcastillor03@gmail.com>
2022-08-17 21:40:48 -0500

    version 2 - Se agregan clase productos

--- src/repositorio_ejemplo/Repositorio_Ejemplo.java
+++ src/repositorio_ejemplo/Repositorio_Ejemplo.java
@@ -7,6 +7,7 @@ public class Repositorio_Ejemplo {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Probando repositorio");
+        System.out.println("Probando la segunda version del repositorio");
     }
     
 }
